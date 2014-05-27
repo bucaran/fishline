8a3d7d1c8a542a5fe5675a52db28c593584a9731
Eric Roseman <eric.roseman@colorado.edu>
2014-05-27 12:12:09 -0600

    themes/lolprompt/fish_prompt.fish adjusted username hostname and path parsing error status now reports ![n] instead of exit(n)

--- themes/lolprompt/fish_prompt.fish
+++ themes/lolprompt/fish_prompt.fish
@@ -1,12 +1,12 @@
 # name:		lolprpmpt
-# requires:	git, hostname, tr, wc, whoami
+# requires:	git, hostname, sed, tr, wc
 # thanks:	mostly copied from the robbyrussell theme
 # 		inspired by lolcat python script and bobthefish theme
 
 function fish_prompt
 
 	# store the previous command return status for later
-	set -l cmdsts $status
+	set -l prev_status $status
 
 	# color settings
 	set -l normal	(set_color normal)
@@ -18,21 +18,22 @@ function fish_prompt
 	set -l red	(set_color red)
 
 	# set the user, short hostname (non-fully qualified domain name)
-	# and current path in the standard ssh style format
-	set -l me	$purple	(whoami)	$normal	'@'
-	set -l nfqdn	$blue	(hostname -s)	$normal	':'
-	set -l cwd	$green	(prompt_pwd)	$normal	' '
+	# and current path (abbreviated home directory ~ ) in the standard
+	# ssh style format
+	set -l uname	$normal ''	$purple	$USER					$normal	'@'
+	set -l hname	$normal	''	$blue	(hostname | sed 's/\..*//' ^/dev/null)	$normal	':'
+	set -l cwd	$normal	''	$green	(pwd | sed "s,$HOME,~," ^/dev/null)	$normal	' '
 
 	# the git bits
 	if set -l branch (git rev-parse --abbrev-ref HEAD ^/dev/null)
 		set -l files (git status -s --ignore-submodules ^/dev/null | wc -l | tr -d ' ')
 		test $files -ne 0; and set -l dirty $normal ':' $red $files
-		set git $yellow 'git' $normal '(' $orange $branch $dirty $normal ') '
+		set git $yellow 'git' $normal '[' $orange $branch $dirty $normal '] '
 	end
 
 	# display the return value of the last command
 	# only if the command produced an error
-	test $cmdsts -ne 0; and set -l error $normal 'exit(' $red $cmdsts $normal ') '
+	test $prev_status -ne 0; and set -l error $normal '![' $red $prev_status $normal '] '
 
 	# hashtag the prompt for root
 	switch $USER
@@ -43,5 +44,5 @@ function fish_prompt
 	end
 
 	# finally print the prompt
-	echo -n -s $me $nfqdn $cwd $git $error $prompt
+	echo -n -s $uname $hname $cwd $git $error $prompt
 end
