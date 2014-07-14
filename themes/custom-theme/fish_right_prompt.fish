4f8513f29ca7fdd3fd32315c5625dd02e617ce66
jeremiejig <me@jeremiejig.fr>
2014-07-14 19:46:18 -0400

    Moving function __system_load from theme to plugin

--- themes/custom-theme/fish_right_prompt.fish
+++ themes/custom-theme/fish_right_prompt.fish
@@ -4,14 +4,6 @@ set -l oh_my_fish_path (status -f|sed 's-/custom.*\?$--')
 set -l theme_path $oh_my_fish_path/themes/$fish_custom_theme/fish_right_prompt.fish
 set -l custom_path $oh_my_fish_path/custom/themes/$fish_custom_theme/fish_right_prompt.fish
 
-function __load_system_fish_right_prompt
-  if test -f /etc/fish/functions/fish_right_prompt.fish
-	 source /etc/fish/functions/fish_right_prompt.fish
-  else if test -f /usr/share/fish/functions/fish_right_prompt.fish
-  	 source /usr/share/fish/functions/fish_right_prompt.fish
-  end
-end
-
 # Assuming that if COLORTERM isn't set we are in a tty
 # In a tty fallback to system fish_prompt
 if not set -q COLORTERM
