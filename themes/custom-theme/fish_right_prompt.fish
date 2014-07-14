d4b758eb677d44bd37e1713ad8e3d583d8285cc3
jeremiejig <me@jeremiejig.fr>
2014-07-14 19:29:22 -0400

    moving logic code from theme to plugin Adding 'on fish_prompt' function 	* will perform the check 	* Will print a message if newer vanilla version detected

--- themes/custom-theme/fish_right_prompt.fish
+++ themes/custom-theme/fish_right_prompt.fish
@@ -1,40 +1,8 @@
 
-# Testing the current version of the theme against the current custom
-# Print a warning if the file has been changed.
-# Assuming classic installation, oh-my-fish is a git repo
 set -l oh_my_fish_path (status -f|sed 's-/custom.*\?$--')
-set -lx GIT_DIR "$oh_my_fish_path/.git"
-set -lx GIT_WORK_TREE "$oh_my_fish_path"
 
 set -l theme_path $oh_my_fish_path/themes/$fish_custom_theme/fish_right_prompt.fish
 set -l custom_path $oh_my_fish_path/custom/themes/$fish_custom_theme/fish_right_prompt.fish
-
-set -l theme_version __customtheme_theme_right_prompt_version_$fish_custom_theme
-set -l custom_version __customtheme_custom_right_prompt_version_$fish_custom_theme
-
-if git status > /dev/null ^&1
-  if test -f "$theme_path" ; and test -f "$custom_path"
-    set -l current_theme_version (git log -n 1 -- $theme_path | grep commit)
-    set -l current_custom_version (command ls -lon --time-style +%s "$custom_path"|\
-    sed 's- /.*$--;s/^\([^0-9]*[0-9]\+\)\{3\} //')
-
-    if not set -q $theme_version 
-  	   or not set -q $custom_version
-
-	   set -U $theme_version $current_theme_version
-	   set -U $custom_version $current_custom_version
-    else if not test $$theme_version = $current_theme_version
-	   if test $$custom_version = $current_custom_version
-  		  set_color red
-	  	  echo -e "The theme '$fish_custom_theme' has been changed.\nAn update to your fish_right_prompt.fish is necessary."
-  		  set_color normal
-	   else
-		  set -U $theme_version $current_theme_version
-  		  set -U $custom_version $current_custom_version
-	   end
-    end
-  end
-end
 
 function __load_system_fish_right_prompt
   if test -f /etc/fish/functions/fish_right_prompt.fish
