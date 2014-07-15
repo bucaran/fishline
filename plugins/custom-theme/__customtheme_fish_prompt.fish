3d9b3b4bb6b4d54ad36c3add7a681f4b641b305d
jeremiejig <me@jeremiejig.fr>
2014-07-15 00:13:16 -0400

    Moving some var to global scope. Improve time execution by 2.

--- plugins/custom-theme/__customtheme_fish_prompt.fish
+++ plugins/custom-theme/__customtheme_fish_prompt.fish
@@ -4,17 +4,16 @@ function __customtheme_fish_prompt
   # Testing the current version of the theme against the current custom
   # Print a warning if the file has been changed.
   # Assuming classic installation, oh-my-fish is a git repo
-  set -l oh_my_fish_path (status -f|command sed 's-/custom.*\?$--')
-  set -lx GIT_DIR "$oh_my_fish_path/.git"
-  set -lx GIT_WORK_TREE "$oh_my_fish_path"
+  set -lx GIT_DIR "$__customtheme_oh_my_fish_path/.git"
+  set -lx GIT_WORK_TREE "$__customtheme_oh_my_fish_path"
 
-  set -l theme_path $oh_my_fish_path/themes/$fish_custom_theme/fish_prompt.fish
-  set -l custom_path $oh_my_fish_path/custom/themes/$fish_custom_theme/fish_prompt.fish
+  set -l theme_path $__customtheme_oh_my_fish_path/themes/$fish_custom_theme/fish_prompt.fish
+  set -l custom_path $__customtheme_oh_my_fish_path/custom/themes/$fish_custom_theme/fish_prompt.fish
 
   set -l theme_version __customtheme_theme_prompt_version_$fish_custom_theme
   set -l custom_version __customtheme_custom_prompt_version_$fish_custom_theme
 
-  if command git status > /dev/null ^&1
+  if set -q __customtheme_git_check_ok
     if test -f "$theme_path" ; and test -f "$custom_path"
       set -l current_theme_version (command git log -n 1 -- "$theme_path" | grep commit)
       set -l current_custom_version (command ls -lon --time-style +%s "$custom_path"|\
