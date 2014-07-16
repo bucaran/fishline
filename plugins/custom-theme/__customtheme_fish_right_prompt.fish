5f06be5ad2bba8a832bf2eef3e9618200388243e
jeremiejig <me@jeremiejig.fr>
2014-07-15 23:11:14 -0400

    changing when to check custom file

--- plugins/custom-theme/__customtheme_fish_right_prompt.fish
+++ plugins/custom-theme/__customtheme_fish_right_prompt.fish
@@ -15,26 +15,23 @@ function __customtheme_fish_right_prompt
 
   if set -q __customtheme_git_check_ok
     if test -f "$theme_path" ; and test -f "$custom_path"
-      set -l current_theme_version (command git log -n 1 -- "$theme_path" | grep commit)
-      set -l current_custom_version (command ls -lon --time-style +%s "$custom_path"|\
-      sed 's- /.*$--;s/^\([^0-9]*[0-9]\+\)\{3\} //')
+      set -l current_theme_version (command git log -n 1 -- "$theme_path" | command grep commit)
 
       if not set -q $theme_version 
-		  or not set -q $custom_version
   		  set -U $theme_version $current_theme_version
-  		  set -U $custom_version $current_custom_version
 
 		# The theme has been changed !
       else if not test $$theme_version = $current_theme_version
-		  if test $$custom_version = $current_custom_version
+  		  set -l current_custom_version (command ls -lon --time-style +%s "$custom_path"|\
+  		  command sed 's- /.*$--;s/^\([^0-9]*[0-9]\+\)\{3\} //')
+		  if not set -q $custom_version
+  			 set -U $custom_version $current_custom_version
+		  else if test $$custom_version = $current_custom_version
 			 set retval 1
 		  else
 			 set -U $theme_version $current_theme_version
-			 set -U $custom_version $current_custom_version
+			 set -e $custom_version
 		  end
-		# The Theme hasn't been changed, but the custom file has.
-		else if test $$custom_version != $current_custom_version
-  		  set -U $custom_version $current_custom_version
       end
     end
   end
