03b5c11c380c9e7c5c9ddfbfe073b423c80c9a99
Pierpaolo Frasa <pfrasa@gmail.com>
2014-11-07 03:13:56 +0100

    display ruby version before git status

--- themes/bira/fish_prompt.fish
+++ themes/bira/fish_prompt.fish
@@ -53,8 +53,8 @@ function fish_prompt
   echo -n (set_color white)"╭─"(set_color normal)
   __user_host
   __current_path
-  __git_status
   __ruby_version
+  __git_status
   echo -e ''
   echo (set_color white)"╰─"(set_color --bold white)"\$ "(set_color normal)
 end
