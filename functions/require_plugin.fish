358ae2e56247e3dbc556bf6f293a74845e6c25b2
Jorge Bucaran <jbucaran@me.com>
2015-01-04 07:37:30 +0900

    Remove old require_plugin and require_tree.

--- functions/require_plugin.fish
+++
@@ -1,9 +0,0 @@
-# Load plugin and dependencies.
-# @params <plugin_name>[..]
-function require_plugin -d "Load a plugin list and dependencies."
-  for plugin in $argv
-    _fish_add_plugin $plugin
-    _fish_add_completion $plugin
-    _fish_source_plugin_load_file $plugin
-  end
-end
