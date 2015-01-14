2db16884a3a72586b20b31ec29f9dd6a5c716f44
Jorge Bucaran <jbucaran@me.com>
2015-01-14 19:43:07 +0900

    Rewording in list.erase.spec test descriptions.

--- plugins/fish-spec/spec/list.erase.spec.fish
+++ plugins/fish-spec/spec/list.erase.spec.fish
@@ -1,51 +1,51 @@
 import plugins/fish-spec
 
-function describe_list_erase -d "Testing Oh-My-Fish `list.erase` function..."
+function describe_list_erase -d "fish-spec/list.erase"
   function before_each
     set -g nums_until_10 1 2 3 4 5 6 7 8 9 10
     set -g odds_until_10 1 3 5 7 9
   end
 
-  function it_erases_one_element -d "It erases an element from a list."
+  function it_erases_one_element -d "erases an element from a list."
     list.erase 1 nums_until_10
     expect $nums_until_10 --to-not-contain 1
   end
 
   function it_erases_one_element_without_from_option \
-    -d "It erases an element with --from option."
+    -d "erases an element with --from option."
     list.erase 1 --from nums_until_10
     expect $nums_until_10 --to-not-contain 1
   end
 
   function it_erases_one_element_from_multiple_lists \
-    -d "It erases an element from multiple lists."
+    -d "erases an element from multiple lists."
     list.erase 1 --from nums_until_10 odds_until_10
     expect $nums_until_10 --to-not-contain 1
       and expect $odds_until_10 --to-not-contain 1
   end
 
   function it_erases_one_element_from_multiple_lists_when_only_one_has_the_element \
-    -d "It erases an element from multiple lists when only one has the element."
+    -d "erases an element from multiple lists when only one has the element."
     list.erase 2 --from nums_until_10 odds_until_10
     expect $nums_until_10 --to-not-contain 2
   end
 
   function it_erases_multiple_elements \
-    -d "It erases multiple elements."
+    -d "erases multiple elements."
     list.erase 1 2 nums_until_10
     expect $nums_until_10 --to-not-contain 1
       and expect $nums_until_10 --to-not-contain 2
   end
 
   function it_erases_multiple_elements_with_from_syntax \
-    -d "It erases multiple elements with --from option."
+    -d "erases multiple elements with --from option."
     list.erase 1 2 --from nums_until_10
     expect $nums_until_10 --to-not-contain 1
       and expect $nums_until_10 --to-not-contain 2
   end
 
   function it_erases_multiple_elements_from_multiple_lists \
-    -d "It erases multiple elements from multiple lists."
+    -d "erases multiple elements from multiple lists."
     list.erase 1 2 --from nums_until_10 odds_until_10
     expect $nums_until_10 --to-not-contain 1
       and expect $nums_until_10 --to-not-contain 2
