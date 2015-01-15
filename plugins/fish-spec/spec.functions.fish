5c7f594707bf7a17f45227ad93a88fd7a3d1315c
Jorge Bucaran <jbucaran@me.com>
2015-01-15 21:58:29 +0900

    add support for grep options to spec.functions

--- plugins/fish-spec/spec.functions.fish
+++ plugins/fish-spec/spec.functions.fish
@@ -2,15 +2,20 @@
 #   spec.functions - retrieve functions in the global scope that match ^key
 #
 # SYNOPSIS
-#   spec.functions <key>
+#   spec.functions [OPTIONS] <key>
+#
+# OPTIONS
+#   See grep(1)
 #
 # EXAMPLES
 #   spec.functions "describe_"
-#   spec.functions "before_"
+#   spec.functions -q "before_"
 #/
 function spec.functions -a key
+  set -l key $argv[-1]
+  set -e argv[-1]
   if [ -n "$key" ]
     # Skip empty strings to avoid fetching all global functions.
-    functions -n | grep \^"$key"
+    functions -n | grep $argv \^"$key"
   end
 end
